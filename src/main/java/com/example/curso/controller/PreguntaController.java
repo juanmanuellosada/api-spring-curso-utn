9f0aba5739ffa684d6a90aff44bb285b34b14779
osirisdavid <davidosirisayala@gmail.com>
2024-09-04 21:24:19 -0300

    se agrega dependencias modelmapper, validation & actuator

--- src/main/java/com/example/curso/controller/PreguntaController.java
+++ src/main/java/com/example/curso/controller/PreguntaController.java
@@ -1,6 +1,13 @@
 package com.example.curso.controller;
 
+import com.example.curso.entity.Pregunta;
+import com.example.curso.services.PreguntaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.awt.print.Pageable;
 
@@ -8,6 +15,15 @@ import java.awt.print.Pageable;
 @RequestMapping("/api/v1/pregunta")
 public class PreguntaController {
 
+    private PreguntaService preguntaService;
+    public PreguntaController(PreguntaService preguntaService){
+        this.preguntaService = preguntaService;
+    }
 
+    @GetMapping
+    public ResponseEntity<?> getOne(@PathVariable(name = "id") Long id){
+        Pregunta pregunta = preguntaService.getOne(id);
+        return new ResponseEntity<>(pregunta, HttpStatus.OK);
+    }
 
 }
